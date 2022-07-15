d515b7e849f6c576a179c63cd67950c8b047c0a6
olimjonsn230 <olimjonsn230@gmail.com>
2022-07-15 08:23:33 +0500

    done

--- basic_func13.py
+++ basic_func13.py
@@ -1,3 +1,5 @@
 # Create a function called main.
 # Create function argument a.
 # Returns the square root of a number.
+def main(a):
+    return a**0.5
--- basic_func18.py
+++ basic_func18.py
@@ -2,3 +2,6 @@
 # Create function arguments a.
 # Round the result to 2 decimal places.
 # Assign the value pi to the variable "a" and return.
+def main(a):
+    a = round(a,2)
+    return a
